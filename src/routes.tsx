50557fd97b59531daaf336ebf2694c7f0ad43af7
helder-hanka <hfernandes238@gmail.com>
2024-12-04 02:16:53 +0100

    Add nav link and routes login signup: Add styles

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,21 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./screens/dashboard/Dashboard";
 
-const router = createBrowserRouter([{ path: "/", element: <Dashboard /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    children: [
+      {
+        errorElement: <i>Error page</i>,
+        children: [
+          { index: true, element: <i>Page A construre</i> },
+          { path: "login/", element: <h1>Login</h1> },
+          { path: "signup", element: <h1>Signup</h1> },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 import "./Dashboard.scss";
 
 const Dashboard: React.FC = () => {
   return (
     <>
-      <nav id="sidebar"></nav>
-      <section id="detail"></section>
+      <nav id="sidebar">
+        <ul>
+          <li>
+            <NavLink to="login">Connexion Admin</NavLink>
+          </li>
+          <li>
+            <NavLink to="signup">Creéation Admin</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <section id="detail">
+        <Outlet />
+      </section>
     </>
   );
 };
