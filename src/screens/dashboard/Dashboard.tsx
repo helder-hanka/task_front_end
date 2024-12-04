c0e9b00f7bf46beac117f89788bc62e6da007a88
helder-hanka <hfernandes238@gmail.com>
2024-12-04 01:19:51 +0100

    Create dashbord

---
+++ src/screens/dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Dashboard.scss";
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <nav id="sidebar"></nav>
+      <section id="detail"></section>
+    </>
+  );
+};
+
+export default Dashboard;
