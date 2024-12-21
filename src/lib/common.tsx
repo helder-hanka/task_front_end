b82a2a1210b3b7e828bdab46d97abba812a7d4d1
helder-hanka <hfernandes238@gmail.com>
2024-12-21 08:14:42 +0100

    update getInLocalStorage func and create removeUserToken func

--- src/lib/common.tsx
+++ src/lib/common.tsx
@@ -25,23 +25,34 @@ const saveUserToLocalStorage = (token: string, userId: string): void => {
   }
 };
 
-const getInLocalStorage = () => {
+export const getInLocalStorage = () => {
   const defaultUser = { token: false, userId: false };
+  const userStorage = localStorage.getItem("user");
   try {
-    let userStorage = localStorage.getItem("user");
-
     if (!userStorage) {
       console.error("Error parsing localStorage Data");
       return defaultUser;
     }
-    JSON.parse(userStorage) as { token: string; userId: string };
-    return defaultUser;
+    const parseData = JSON.parse(userStorage) as {
+      token: string;
+      userId: string;
+    };
+    return parseData;
   } catch (error) {
     console.error("Error parsing localStorage Data", error);
     return defaultUser;
   }
 };
 
+export const removeUserToken = () => {
+  try {
+    localStorage.removeItem("user");
+    return true;
+  } catch (error) {
+    console.error("Error removing user token", error);
+    return false;
+  }
+};
 export const API = {
   login: async (credentials: LoginReq): Promise<any> => {
     try {
