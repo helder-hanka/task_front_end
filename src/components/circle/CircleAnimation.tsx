3ab97da9a93713a81b9227fb3d3bcad33569e638
helder-hanka <hfernandes238@gmail.com>
2024-12-21 09:50:58 +0100

    Create circle animattion

---
+++ src/components/circle/CircleAnimation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CircleAnimation.scss";
+function CircleAnimation() {
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="circle"></div>
+        <span>Connecté</span>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default CircleAnimation;
